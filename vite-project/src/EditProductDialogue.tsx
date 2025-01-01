2892afceddfbea952859da1dcc3ae58b15f4a4b7
arvind97veative <aravind.pandit@veative.com>
2025-01-01 08:36:20 +0530

    added trim for whitespace to category while editing

--- vite-project/src/EditProductDialogue.tsx
+++ vite-project/src/EditProductDialogue.tsx
@@ -37,7 +37,7 @@ const EditProductDialog: React.FC<EditProductDialogProps> = ({
   const updatedProduct = useMemo(
     () => ({
       ...product,
-      category,
+      category: category.trim(),
       quantity,
       price: `$${price}`,
       value: finalValue,
