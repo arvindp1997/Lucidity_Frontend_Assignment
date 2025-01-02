b70dedc74bab61507c203762001b9771cd3b10d5
arvind97veative <aravind.pandit@veative.com>
2025-01-02 11:21:15 +0530

    made widget component mobile responsive

--- vite-project/src/ProductStatsWidget.tsx
+++ vite-project/src/ProductStatsWidget.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Stack, Box, Typography } from '@mui/material';
-import { brown } from '@mui/material/colors';
+import React from "react";
+import { Stack, Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { brown } from "@mui/material/colors";
 
 interface ProductStat {
   icon: React.ReactNode;
@@ -13,26 +13,43 @@ interface ProductStatsWidgetProps {
 }
 
 const ProductStatsWidget: React.FC<ProductStatsWidgetProps> = ({ productStats }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <Stack direction="row" justifyContent="space-between">
+    <Stack
+      direction={isSmallScreen ? "column" : "row"}
+      justifyContent="space-between"
+      spacing={isSmallScreen ? 2 : 4}
+    >
       {productStats.map((item) => (
         <Box
           key={item.title}
           display="flex"
           flexDirection="row"
-          gap={4}
-          width={350}
-          height={120}
+          alignItems="center"
+          gap={isSmallScreen ? 2 : 4}
+          width={isSmallScreen ? "100%" : 350}
+          height={isSmallScreen ? 100 : 120}
           sx={{
             backgroundColor: brown[900],
             borderRadius: "5%",
-            padding: "20px",
+            padding: isSmallScreen ? "10px" : "20px",
           }}
         >
           {item.icon}
-          <Stack direction="column" gap={2}>
-            <Typography>{item.title}</Typography>
-            <Typography variant="h4" fontSize={40}>
+          <Stack direction="column" gap={1}>
+            <Typography
+              variant={isSmallScreen ? "body2" : "body1"}
+              fontSize={isSmallScreen ? 14 : 16}
+            >
+              {item.title}
+            </Typography>
+            <Typography
+              variant="h4"
+              fontSize={isSmallScreen ? 24 : 40}
+              sx={{ fontWeight: isSmallScreen ? 500 : 700 }}
+            >
               {item.value}
             </Typography>
           </Stack>
